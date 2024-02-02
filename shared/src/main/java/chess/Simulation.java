e0aa5138440c53f77cdbce3402fe1fe02542aace
Bailee <baileebyu@gmail.com>
2024-02-02 16:11:45 -0700

    SimulateMoves function written. Not tested; waiting on simulateMove function.

--- shared/src/main/java/chess/Simulation.java
+++ shared/src/main/java/chess/Simulation.java
@@ -4,14 +4,35 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
+import static chess.ChessPiece.PieceType.KING;
+
 
 public class Simulation {
 
     public static boolean simulateMoves(ChessBoard board, ChessMove move) {
-        ChessBoard simBoard = board;
-        // find the position of the king
+        ChessBoard simBoard = new ChessBoard();
+        simBoard = board;
+        ChessPosition kingPosition = null;
+        ChessPiece tempPiece;
+        ChessPosition tempPosition;
+        ChessPiece protagonist = board.getPiece(move.getStartPosition());
+        ChessGame.TeamColor teamColor = protagonist.getTeamColor();
+        boolean result = true;
 
         // make the move on simBoard
+        simBoard.addPiece(move.getEndPosition(), simBoard.getPiece(move.getStartPosition()));
+        simBoard.addPiece(move.getStartPosition(), null);
+
+        // find the position of the home team king
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                tempPiece = simBoard.getPiece(new ChessPosition(i,j));
+                if ( tempPiece.getPieceType() == KING && tempPiece.getTeamColor() == teamColor) {
+                    kingPosition = new ChessPosition(i,j);
+                    break;
+                }
+            }
+        }
 
 
         // iterate through every enemy piece on simBoard
@@ -19,12 +40,23 @@ public class Simulation {
         // keep going while the answer is true
         // if we get a false, return false
         // if it all passes through true, return true
+        while (result) {
+            for (int i = 1; i <= 8; i++) {
+                for (int j = 1; j <= 8; j++) {
+                    tempPosition = new ChessPosition(i, j);
+                    tempPiece = simBoard.getPiece(tempPosition);
+                    if (tempPiece.getTeamColor() != teamColor) {
+                        result = simulateMove(simBoard, tempPosition, kingPosition);
+                    }
+                }
+            }
+        }
 
 
-        return true;
+        return result;
     }
 
-    private boolean simulateMove(ChessBoard simBoard, ChessPosition enemyPosition, ChessPosition kingPosition) {
+    private static boolean simulateMove(ChessBoard simBoard, ChessPosition enemyPosition, ChessPosition kingPosition) {
         Collection<ChessMove> moves = new ArrayList<>();
 
         moves = PieceMovesCalc.pieceMoves(simBoard, enemyPosition);
@@ -34,7 +66,7 @@ public class Simulation {
         // if we get through true, return true
         // while (status == true) {} probably
 
-        return true; // fix this
+        return false; // fix this
     }
 
 }
