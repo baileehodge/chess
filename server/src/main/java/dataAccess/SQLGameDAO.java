28ca084df65cdeea08167d20a8bdcbf9a6faf123
Bailee <baileebyu@gmail.com>
2024-03-05 21:12:29 -0700

    3/5/24 EOD

--- server/src/main/java/dataAccess/SQLGameDAO.java
+++ server/src/main/java/dataAccess/SQLGameDAO.java
@@ -11,7 +11,8 @@ import static java.sql.Types.NULL;
 public class SQLGameDAO implements GameDAO{
     public void clearGames() throws DataAccessException {
         var statement = "TRUNCATE pet";
-        executeUpdate(statement);
+        // executeUpdate(statement);
+        // see prototype
     }
 
     public Collection<GameData> listGames() throws DataAccessException {
@@ -30,29 +31,31 @@ public class SQLGameDAO implements GameDAO{
         return null;
     }
 
-    private int executeUpdate(String statement, Object... params) throws ResponseException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    if (param instanceof String p) ps.setString(i + 1, p);
-                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
-                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
-                    else if (param == null) ps.setNull(i + 1, NULL);
-                }
-                ps.executeUpdate();
+// prototype, copied from pet shop
 
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-
-                return 0;
-            }
-        } catch (SQLException e) {
-            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
-        } catch (DataAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    private int executeUpdate(String statement, Object... params) throws ResponseException {
+//        try (var conn = DatabaseManager.getConnection()) {
+//            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+//                for (var i = 0; i < params.length; i++) {
+//                    var param = params[i];
+//                    if (param instanceof String p) ps.setString(i + 1, p);
+//                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+//                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
+//                    else if (param == null) ps.setNull(i + 1, NULL);
+//                }
+//                ps.executeUpdate();
+//
+//                var rs = ps.getGeneratedKeys();
+//                if (rs.next()) {
+//                    return rs.getInt(1);
+//                }
+//
+//                return 0;
+//            }
+//        } catch (SQLException e) {
+//            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+//        } catch (DataAccessException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
