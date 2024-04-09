6cc89590e5e048b129644a9f67ea9c2d76066a75
Bailee <baileebyu@gmail.com>
2024-04-08 20:03:52 -0600

    WebSocketHandler onMessage

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,12 +1,18 @@
 package server.websocket;
 
+import WebSocketMessages.userCommands.UserGameCommand;
+import com.google.gson.Gson;
 import com.mysql.cj.Session;
+import org.eclipse.jetty.server.HttpChannelState;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 
 import java.util.Map;
+
+import static WebSocketMessages.userCommands.UserGameCommand.CommandType.*;
+import static java.lang.System.exit;
 
 public class WebSocketHandler {
     final WebSocketSessions sessions;
@@ -31,10 +37,19 @@ public class WebSocketHandler {
     }
 
     @OnWebSocketMessage
-    void onMessage(Session session, String str) {
+    void onMessage(Session session, String message) {
         // 1. Determine message type
         // 2. Call one of the following methods to process the message
         //    you can also call a service method, probably GameService
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case JOIN_PLAYER -> joinPlayer(message);
+            case JOIN_OBSERVER -> joinObserver(message);
+            case MAKE_MOVE -> makeMove(message);
+            case LEAVE -> leaveGame(message);
+            case RESIGN -> resignGame(message);
+            // set a default?
+        }
     }
     // assuming these are void for now
     void joinPlayer(String message) {
